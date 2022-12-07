d0c242b19548663b0067670edde72fac442e68ab
Tatiane Patriota <tatianepatriota1@gmail.com>
2022-12-07 16:25:01 -0300

    add estilo global com styled components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./reset.css";
 import App from "./App";
+import { createGlobalStyle } from "styled-components";
 
+const StyledGlobal = createGlobalStyle`
+body {
+  margin: 0;
+  padding: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+    monospace;
+}
+
+a {
+  text-decoration: none;
+  color: #000000;
+}
+
+`;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <StyledGlobal />
     <App />
   </React.StrictMode>
 );
