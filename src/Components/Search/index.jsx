8451f7b27f5678acbb0221ba83202f0b26cad85c
Tatiane Patriota <tatianepatriota1@gmail.com>
2022-12-08 09:35:05 -0300

    removendo altura

--- src/Components/Search/index.jsx
+++ src/Components/Search/index.jsx
@@ -1,14 +1,13 @@
 import Input from "../Input";
 import styled from "styled-components";
 import { useState } from "react";
-import { books } from "./dadosPesquisa";
+import { books } from "./researchData";
 
 const StyledSearch = styled.section`
   background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
   color: #fff;
   text-align: center;
   padding: 85px 0;
-  height: 270px;
   width: 100%;
 `;
 
