3a8a0f982a8aa15d6935c89ad6d7db27ccc6df00
Tatiane Patriota <tatianepatriota1@gmail.com>
2022-12-08 07:05:06 -0300

    add map e onblur na funcao

--- src/Components/Search/index.jsx
+++ src/Components/Search/index.jsx
@@ -1,5 +1,7 @@
 import Input from "../Input";
 import styled from "styled-components";
+import { useState } from "react";
+import { books } from "./dadosPesquisa";
 
 const StyledSearch = styled.section`
   background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
@@ -23,12 +25,46 @@ const StyledCaption = styled.h3`
   margin-bottom: 40px;
 `;
 
+const StyledSearchResult = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+  cursor: pointer;
+  p {
+    width: 200px;
+  }
+  img {
+    width: 130px;
+  }
+  &:hover {
+    border: 1px solid white;
+  }
+`;
+
 function Search() {
+  const [researchedBooks, setResearchedBooks] = useState([]);
+
   return (
     <StyledSearch>
       <StyledTitle>Já sabe por onde começar?</StyledTitle>
       <StyledCaption>Encontre seu livro em nossa estante</StyledCaption>
-      <Input placeholder="Escreva sua próxima leitura" />
+      <Input
+        placeholder="Escreva sua próxima leitura"
+        onBlur={(event) => {
+          const typedText = event.target.value;
+          const searchResult = books.filter((book) =>
+            book.nome.includes(typedText)
+          );
+          setResearchedBooks(searchResult);
+        }}
+      />
+      {researchedBooks.map((book) => (
+        <StyledSearchResult>
+          <p>{book.nome}</p>
+          <img src={book.src} alt={book.nome} />
+        </StyledSearchResult>
+      ))}
     </StyledSearch>
   );
 }
