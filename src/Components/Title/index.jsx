6b06db61904af64fbcacf3239b09b95d93116d73
Tatiane Patriota <tatianepatriota1@gmail.com>
2022-12-08 14:58:27 -0300

    add props no title

--- src/Components/Title/index.jsx
+++ src/Components/Title/index.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const StyledTitle = styled.h2`
   width: 100%;
-  padding: 30px 0;
+  padding: ${(props) => props.preenchimento || "30px 0"};
   background-color: #fff;
   color: ${(props) => props.cor || "#000000"};
   font-size: ${(props) => props.tamanhoFonte || "18px"};
